5e610ea10252bbdab1351969d69f8fca62ce1d18
bobomurod <bobomurod.boboev@yandex.ru>
2019-03-27 19:54:57 +0500

    new App.js file

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+    createStackNavigator,
+    createAppContainer
+  } from 'react-navigation';
+import Home from './components/Home'
+import Profile from './components/Profile'
+import Feed from './components/Feed'
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Hello navigation</Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const RootStack = createStackNavigator({
+  Home: {
+    screen: Home
   },
+  Profile: {
+    screen: Profile
+  },
+  Feed: {
+      screen: Feed
+  }
 });
+
+const App = createAppContainer(RootStack);
+
+export default App;
