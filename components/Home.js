25d73d16514e01a81b32522b6cf0d771dc705783
bobomurod <bobomurod.boboev@yandex.ru>
2019-03-27 19:55:15 +0500

    Button added

--- components/Home.js
+++ components/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default class Home extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>Hello ftom Home</Text>
+        <Button title="NAv" onPress={ ()=>this.props.navigation.navigate('Profile') }/>
+        <Button title="NAv" onPress={ ()=>this.props.navigation.navigate('Feed') }/>
       </View>
     );
   }
